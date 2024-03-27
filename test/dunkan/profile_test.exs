c460dd4ace8d6230f8bdeac3ff17f4c7c50b46ab
rim-777 <rails.moss@gmail.com>
2024-03-27 12:27:15 +0000

    test profile changeset

--- test/dunkan/profile_test.exs
+++ test/dunkan/profile_test.exs
@@ -5,7 +5,7 @@ defmodule Dunkan.ProfileTest do
   alias Dunkan.Profile
 
   describe "changeset" do
-    @min_attrs %{
+    @min_valid_attrs %{
       displayed_name: "Timo Moss",
       type: "player"
     }
@@ -16,7 +16,35 @@ defmodule Dunkan.ProfileTest do
                  type: :player
                },
                valid?: true
-             } = Profile.changeset(%Profile{}, @min_attrs)
+             } = Profile.changeset(%Profile{}, @min_valid_attrs)
+    end
+
+    test "changeset/2 with missing type" do
+      missing_type_attrs = @min_valid_attrs |> Map.reject(fn {k, _v} -> k == :type end)
+
+      assert %Ecto.Changeset{
+               valid?: false,
+               errors: [type: {"can't be blank", [validation: :required]}]
+             } = Profile.changeset(%Profile{}, missing_type_attrs)
+    end
+
+    test "changeset/2 with incorrect type" do
+      incorrect_type_attrs = @min_valid_attrs |> Map.replace(:type, :wrong_type)
+
+      assert %Ecto.Changeset{
+               valid?: false,
+               errors: [type: {"is invalid", _reason}]
+             } = Profile.changeset(%Profile{}, incorrect_type_attrs)
+    end
+
+    test "changeset/2 with missing displayed_name" do
+      missing_displayed_name_attrs =
+        @min_valid_attrs |> Map.replace(:displayed_name, nil)
+
+      assert %Ecto.Changeset{
+               valid?: false,
+               errors: [displayed_name: {"can't be blank", [validation: :required]}]
+             } = Profile.changeset(%Profile{}, missing_displayed_name_attrs)
     end
   end
 end
